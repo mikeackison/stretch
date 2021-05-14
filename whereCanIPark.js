00ae7dc9e2438181a9d43ed72a9d0cae993a21ae
Mike Ackison <mike.ackison@shopify.com>
2021-05-14 07:21:06 -0400

    function working, output expected

--- whereCanIPark.js
+++ whereCanIPark.js
@@ -1,4 +1,4 @@
-const whereCanIPark = function (spots, vehicle) {
+const whereCanIPark = (spots, vehicle) => {
   for (let y = 0; y < spots.length; y++) {
     for (let x = 0; x < spots[y].length; x++) {
       const park = spots[y][x];
