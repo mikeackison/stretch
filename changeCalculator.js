79b62ef4d67bc543cd156e61dda6d7615c91157d
Mike Ackison <mike.ackison@shopify.com>
2021-05-24 15:46:47 -0400

    function working, output as expected

---
+++ changeCalculator.js
@@ -0,0 +1,36 @@
+const calculateChange = (total, cash) => {
+  const bills = {
+    twentyDollar: 2000,
+    tenDollar: 1000,
+    fiveDollar: 500,
+    twoDollar: 200,
+    oneDollar: 100,
+    quarter: 25,
+    dime: 10,
+    nickel: 5,
+    penny: 1,
+  };
+
+  let changeObject = {};
+
+  let change = cash - total;
+  console.log(change);
+
+  for (let value in bills) {
+    while (change >= bills[value]) {
+      if (changeObject[value]) {
+        changeObject[value] += 1;
+      } else {
+        changeObject[value] = 1;
+      }
+
+      change = change - bills[value];
+    }
+  }
+
+  return changeObject;
+};
+
+console.log(calculateChange(1787, 2000));
+console.log(calculateChange(2623, 4000));
+console.log(calculateChange(501, 1000));
